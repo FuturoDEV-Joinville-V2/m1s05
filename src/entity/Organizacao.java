19f543b305ea4632301504942cf9b72435331a6f
Davi Saldanha <davisaldanha.ti@gmail.com>
2025-03-12 21:56:26 -0300

    Adicionado atributos e interfaces na classe.

--- src/entity/Organizacao.java
+++ src/entity/Organizacao.java
@@ -1,17 +1,21 @@
 package entity;
 
+import interfaces.Pagamento;
+
 import java.util.ArrayList;
 
-public class Organizacao {
+public class Organizacao implements Pagamento {
     private String nome;
     private ArrayList<Aluno> alunos;
+    private ArrayList<Professor> professores;
 
     public Organizacao() {
     }
 
-    public Organizacao(String nome, ArrayList<Aluno> alunos) {
+    public Organizacao(String nome, ArrayList<Aluno> alunos, ArrayList<Professor> professores) {
         this.nome = nome;
         this.alunos = alunos;
+        this.professores = professores;
     }
 
     public String getNome() {
@@ -30,12 +34,25 @@ public class Organizacao {
         this.alunos = alunos;
     }
 
+    public ArrayList<Professor> getProfessores() {
+        return professores;
+    }
+
+    public void setProfessores(ArrayList<Professor> professores) {
+        this.professores = professores;
+    }
+
     //Método para gerar um relatório dos alunos cadastrados
     public String relatorioAlunos(){
         String resultado = "";
         for(Aluno aluno : alunos){
-            resultado += aluno.exibirDetalhes() + "\n";
+            resultado += aluno.toString() + "\n";
         }
         return resultado;
+    }
+
+    @Override
+    public String realizarPagamento(){
+        return "Pagamento foi efetuado com sucesso!";
     }
 }
